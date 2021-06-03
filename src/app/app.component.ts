f2ca2c0980760d95c82f9701756def9b71b5e765
jun64 <mahaha94@gmail.com>
2021-06-03 13:59:11 +0900

    section 5 before assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [{type: 'server', name: 'TestServer', content: 'Just a test'}];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+    
+  }
+
+  onBlueprintAdded(bluePrintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluePrintData.serverName,
+      content: bluePrintData.serverContent
+    });
+  }
+
+  onChangeFirst(){
+    this.serverElements[0].name = "Changed";
+  }
+
+  onDestroyFirst(){
+    this.serverElements.splice(0, 1);
+  }
 }
